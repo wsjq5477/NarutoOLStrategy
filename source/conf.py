2706b57abb607ae5d9959f6d96849d6293d60940
wsjq5477 <jiqi0503@126.com>
2021-04-24 22:24:44 +0800

    myst_parser更换为CommonMarkParser，以适配ReadOnDocs

--- source/conf.py
+++ source/conf.py
@@ -26,11 +26,16 @@ release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
-
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = [
+    'recommonmark',
+    'sphinx_markdown_tables'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -57,7 +62,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 source_encoding = 'utf-8'
 
